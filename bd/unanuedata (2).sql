5e15e2ce3a749ef91868961f2346178146afaeb2
ElmoSabeDondeVives <jaimesmarin.sergio@gmail.com>
2023-07-23 13:51:24 -0500

    based de datos

---
+++ bd/unanuedata (2).sql
@@ -0,0 +1,567 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 23-07-2023 a las 18:50:41
+-- Versión del servidor: 5.7.33
+-- Versión de PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `unanuedata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especialidades`
+--
+
+CREATE TABLE `especialidades` (
+  `id_especialidad` int(10) NOT NULL,
+  `nombre_especialidad` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `estado_especialidad` tinyint(2) DEFAULT NULL,
+  `fecha_creacion` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `especialidades`
+--
+
+INSERT INTO `especialidades` (`id_especialidad`, `nombre_especialidad`, `estado_especialidad`, `fecha_creacion`) VALUES
+(1, 'CASA', 0, '2023-07-23 00:38:56'),
+(2, 'ADMINISTRACION', 0, '2023-07-23 00:46:15'),
+(3, 'MEDICINA GENERAL', 0, '2023-07-23 10:53:14');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicos`
+--
+
+CREATE TABLE `medicos` (
+  `id_medico` int(10) NOT NULL,
+  `dni_medico` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nombre_medico` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `apellidos_medico` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `direccion_medico` varchar(350) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `telefono_medico` varchar(25) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email_medico` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id_especialidad` int(10) DEFAULT NULL,
+  `estado_medico` tinyint(2) DEFAULT NULL,
+  `fecha_creacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `medicos`
+--
+
+INSERT INTO `medicos` (`id_medico`, `dni_medico`, `nombre_medico`, `apellidos_medico`, `direccion_medico`, `telefono_medico`, `email_medico`, `id_especialidad`, `estado_medico`, `fecha_creacion`) VALUES
+(1, '70616003', 'SERGIO', 'JAIMES', 'MARIN', '916161333', 'JAIMES@GMAIL.COM', 1, 1, '2023-07-22 16:56:27'),
+(2, '70616003', 'SERGIO', 'JAIMES', 'MARIN', '916161333', 'JAIMES@GMAIL.COM', 1, 1, '2023-07-22 16:56:27'),
+(3, '70616003', 'SERGIO', 'JAIMES', 'MARIN', '916161333', 'JAIMES@GMAIL.COM', 1, 1, '2023-07-22 16:56:27');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `menus`
+--
+
+CREATE TABLE `menus` (
+  `id_menu` int(11) NOT NULL,
+  `menu_nombre` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `menu_controlador` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `menu_icono` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `menu_orden` int(11) NOT NULL,
+  `menu_mostrar` tinyint(1) NOT NULL,
+  `menu_estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `menus`
+--
+
+INSERT INTO `menus` (`id_menu`, `menu_nombre`, `menu_controlador`, `menu_icono`, `menu_orden`, `menu_mostrar`, `menu_estado`) VALUES
+(1, 'Login', 'Login', '-', 0, 0, 1),
+(2, 'Panel de Inicio', 'Admin', 'fa fa-dashboard', 1, 0, 1),
+(3, 'Gestión de Menu', 'Menu', 'bx bx-laptop', 2, 1, 1),
+(4, 'Roles de Usuario', 'Rol', 'fa fa-user-secret', 4, 0, 1),
+(5, 'Usuarios', 'Usuario', 'fa fa-user', 5, 0, 1),
+(6, 'Datos Personales', 'Datos', 'fa fa-', 0, 0, 1),
+(7, 'Registro Informacion', 'Registro', 'bx bx-archive', 5, 1, 1),
+(8, 'Medicos', 'Medicos', 'bx bx-plus-medical', 3, 1, 1),
+(9, 'Logistica', 'Logistica', 'bx bx-cog', 4, 1, 1),
+(10, 'Inicio', 'Inicio', 'bx bx-home', 5, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opciones`
+--
+
+CREATE TABLE `opciones` (
+  `id_opcion` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `opcion_nombre` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `opcion_funcion` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `opcion_icono` char(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `opcion_mostrar` tinyint(1) NOT NULL,
+  `opcion_orden` int(11) NOT NULL,
+  `opcion_estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `opciones`
+--
+
+INSERT INTO `opciones` (`id_opcion`, `id_menu`, `opcion_nombre`, `opcion_funcion`, `opcion_icono`, `opcion_mostrar`, `opcion_orden`, `opcion_estado`) VALUES
+(1, 1, 'Inicio de Sesion', 'inicio', '-', 0, 0, 1),
+(2, 2, 'Inicio', 'inicio', 'fa fa-play', 1, 1, 1),
+(3, 2, 'Cerrar Sesión', 'finalizar_sesion', 'fa fa-sign-out', 0, 1, 1),
+(4, 3, 'Gestionar Menús', 'inicio', NULL, 1, 1, 1),
+(5, 3, 'Iconos', 'iconos', NULL, 1, 2, 1),
+(6, 3, 'Accesos por Rol', 'roles', NULL, 0, 0, 1),
+(7, 3, 'Opciones por Menú', 'opciones', NULL, 0, 0, 1),
+(8, 3, 'Gestionar Permisos(breve)', 'gestion_permisos', '', 0, 0, 1),
+(9, 4, 'Gestionar Roles', 'inicio', '', 1, 1, 1),
+(10, 4, 'Accesos por Rol', 'accesos', '', 0, 0, 1),
+(11, 3, 'Gestionar Restricciones(breve)', 'gestion_restricciones', '', 0, 0, 1),
+(12, 5, 'Gestionar Usuarios', 'inicio', '', 1, 1, 1),
+(13, 6, 'Editar Datos', 'editar_datos', '', 0, 0, 1),
+(14, 6, 'Editar Usuario', 'editar_usuario', '', 0, 0, 1),
+(15, 6, 'Cambiar Contraseña', 'cambiar_contrasenha', '', 0, 0, 1),
+(16, 7, 'Registrar', 'registrar', '', 1, 1, 1),
+(17, 8, 'Registro Medico', 'registro', '', 1, 1, 1),
+(18, 8, 'Lista Medicos', 'list', '', 1, 0, 1),
+(19, 8, 'calendar', 'calendar', '', 0, 0, 1),
+(20, 8, 'edit_calendar', 'edit_calendar', '', 0, 0, 1),
+(21, 9, 'Servicios', 'config', '', 1, 1, 1),
+(22, 9, 'Especialidades', 'especialidades', '', 1, 2, 1),
+(23, 10, 'inicio', 'inicio', '', 1, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
+
+CREATE TABLE `permisos` (
+  `id_permiso` int(11) NOT NULL,
+  `id_opcion` int(11) NOT NULL,
+  `permiso_accion` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `permiso_estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `permisos`
+--
+
+INSERT INTO `permisos` (`id_permiso`, `id_opcion`, `permiso_accion`, `permiso_estado`) VALUES
+(1, 1, 'validar_sesion', 1),
+(2, 4, 'guardar_menu', 1),
+(3, 6, 'configurar_relacion', 1),
+(4, 7, 'guardar_opcion', 1),
+(5, 7, 'agregar_permiso', 1),
+(6, 7, 'eliminar_permiso', 1),
+(7, 7, 'configurar_acceso', 1),
+(8, 9, 'guardar_rol', 1),
+(9, 10, 'gestionar_acceso_rol', 1),
+(10, 12, 'guardar_nuevo_usuario', 1),
+(11, 12, 'guardar_edicion_usuario', 1),
+(12, 12, 'guardar_edicion_persona', 1),
+(13, 12, 'restablecer_contrasenha', 1),
+(14, 13, 'guardar_datos', 1),
+(15, 14, 'guardar_usuario', 1),
+(16, 15, 'guardar_contrasenha', 1),
+(17, 17, 'save_medic', 1),
+(18, 19, 'save_calendar_doctor', 1),
+(19, 20, 'save_calendar_edit', 1),
+(20, 21, 'save_service', 1),
+(21, 21, 'save_edit_service', 1),
+(22, 22, 'save_espe', 1),
+(23, 22, 'save_edit_espe', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `personas`
+--
+
+CREATE TABLE `personas` (
+  `id_persona` int(11) NOT NULL,
+  `persona_nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `persona_apellido_paterno` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `persona_apellido_materno` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `persona_nacimiento` date DEFAULT NULL,
+  `persona_telefono` char(15) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `persona_creacion` datetime NOT NULL,
+  `persona_modificacion` datetime NOT NULL,
+  `person_codigo` varchar(40) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `personas`
+--
+
+INSERT INTO `personas` (`id_persona`, `persona_nombre`, `persona_apellido_paterno`, `persona_apellido_materno`, `persona_nacimiento`, `persona_telefono`, `persona_creacion`, `persona_modificacion`, `person_codigo`) VALUES
+(1, 'César José', 'Ruiz', NULL, NULL, NULL, '2020-09-17 00:00:00', '2020-09-17 00:00:00', '010101010101'),
+(2, 'Carlos', 'Melendez', 'Bernuy', '1996-06-14', '965874123', '2020-10-27 18:29:10', '2020-10-27 18:29:10', '1603841350.1786');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `restricciones`
+--
+
+CREATE TABLE `restricciones` (
+  `id_restriccion` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `id_opcion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `rol_nombre` char(20) COLLATE utf8_unicode_ci NOT NULL,
+  `rol_descripcion` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `rol_estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `rol_nombre`, `rol_descripcion`, `rol_estado`) VALUES
+(1, 'Libre', 'Accesos sin inicio de sesión', 1),
+(2, 'SuperAdmin', 'Tiene acceso a la gestión total del sistema', 1),
+(3, 'Admin', 'Gestión del sistema', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles_menus`
+--
+
+CREATE TABLE `roles_menus` (
+  `id_rol_menu` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `roles_menus`
+--
+
+INSERT INTO `roles_menus` (`id_rol_menu`, `id_rol`, `id_menu`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 2, 3),
+(4, 2, 4),
+(5, 2, 5),
+(6, 3, 2),
+(7, 3, 5),
+(8, 2, 6),
+(9, 3, 6),
+(10, 2, 7),
+(12, 2, 8),
+(13, 2, 9),
+(14, 1, 10),
+(15, 2, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id_servicio` int(10) NOT NULL,
+  `nombre_servicio` varchar(80) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `estado_servicio` tinyint(2) DEFAULT NULL,
+  `fecha_crecion` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`id_servicio`, `nombre_servicio`, `estado_servicio`, `fecha_crecion`) VALUES
+(1, 'MEDICINA ', 1, '2023-07-22 23:13:17'),
+(2, 'SANIDAD', 0, '2023-07-22 23:15:02');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turnos`
+--
+
+CREATE TABLE `turnos` (
+  `id_turno` int(10) NOT NULL,
+  `id_medico` int(10) NOT NULL,
+  `intervalo` int(10) NOT NULL,
+  `lunes` tinyint(2) NOT NULL,
+  `martes` int(2) NOT NULL,
+  `miercoles` int(2) NOT NULL,
+  `jueves` int(2) NOT NULL,
+  `viernes` int(2) NOT NULL,
+  `sabado` int(2) NOT NULL,
+  `domingo` int(2) NOT NULL,
+  `estado` int(2) NOT NULL,
+  `fecha_creacion` datetime NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL,
+  `id_servicio` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `turnos`
+--
+
+INSERT INTO `turnos` (`id_turno`, `id_medico`, `intervalo`, `lunes`, `martes`, `miercoles`, `jueves`, `viernes`, `sabado`, `domingo`, `estado`, `fecha_creacion`, `hora_inicio`, `hora_fin`, `id_servicio`) VALUES
+(1, 1, 30, 1, 1, 1, 1, 0, 0, 0, 0, '2023-07-22 21:05:38', '21:05:00', '01:05:00', NULL),
+(2, 2, 12, 0, 0, 1, 1, 0, 0, 0, 0, '2023-07-22 21:10:06', '21:11:00', '00:12:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `usuario_nickname` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_contrasenha` varchar(70) COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_email` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_imagen` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `usuario_estado` tinyint(1) NOT NULL,
+  `usuario_creacion` datetime NOT NULL,
+  `usuario_ultimo_login` datetime NOT NULL,
+  `usuario_ultima_modificacion` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `id_persona`, `id_rol`, `usuario_nickname`, `usuario_contrasenha`, `usuario_email`, `usuario_imagen`, `usuario_estado`, `usuario_creacion`, `usuario_ultimo_login`, `usuario_ultima_modificacion`) VALUES
+(1, 1, 2, 'superadmin', '$2y$10$oPOOOgTUr4zIh511ATm/q.vzsAmxP.e2.vzyEbRn/1pzyWz2oXj0a', 'cesarjose@bufeotec.com', 'media/usuarios/usuario.jpg', 1, '2020-09-17 00:00:00', '2023-07-23 10:41:32', '2020-09-17 00:00:00'),
+(2, 2, 3, 'admin', '$2y$10$8ZxmfjUaJocc1SOYS8vDNufcPgcru5aMiEp4HP9J8KA.7mnlkFfiu', 'carlos@gmail.com', 'media/usuarios/usuario.jpg', 1, '2020-10-27 18:29:10', '2020-10-27 18:29:10', '2020-10-27 18:29:10');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `especialidades`
+--
+ALTER TABLE `especialidades`
+  ADD PRIMARY KEY (`id_especialidad`);
+
+--
+-- Indices de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  ADD PRIMARY KEY (`id_medico`);
+
+--
+-- Indices de la tabla `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indices de la tabla `opciones`
+--
+ALTER TABLE `opciones`
+  ADD PRIMARY KEY (`id_opcion`),
+  ADD KEY `id_menu` (`id_menu`);
+
+--
+-- Indices de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD PRIMARY KEY (`id_permiso`),
+  ADD KEY `id_opcion` (`id_opcion`);
+
+--
+-- Indices de la tabla `personas`
+--
+ALTER TABLE `personas`
+  ADD PRIMARY KEY (`id_persona`);
+
+--
+-- Indices de la tabla `restricciones`
+--
+ALTER TABLE `restricciones`
+  ADD PRIMARY KEY (`id_restriccion`),
+  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `id_opcion` (`id_opcion`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `roles_menus`
+--
+ALTER TABLE `roles_menus`
+  ADD PRIMARY KEY (`id_rol_menu`),
+  ADD KEY `id_rol` (`id_rol`),
+  ADD KEY `id_menu` (`id_menu`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id_servicio`);
+
+--
+-- Indices de la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  ADD PRIMARY KEY (`id_turno`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_persona` (`id_persona`),
+  ADD KEY `id_rol` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `especialidades`
+--
+ALTER TABLE `especialidades`
+  MODIFY `id_especialidad` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  MODIFY `id_medico` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `opciones`
+--
+ALTER TABLE `opciones`
+  MODIFY `id_opcion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT de la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  MODIFY `id_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT de la tabla `personas`
+--
+ALTER TABLE `personas`
+  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `restricciones`
+--
+ALTER TABLE `restricciones`
+  MODIFY `id_restriccion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `roles_menus`
+--
+ALTER TABLE `roles_menus`
+  MODIFY `id_rol_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id_servicio` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  MODIFY `id_turno` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `opciones`
+--
+ALTER TABLE `opciones`
+  ADD CONSTRAINT `opciones_ibfk_1` FOREIGN KEY (`id_menu`) REFERENCES `menus` (`id_menu`);
+
+--
+-- Filtros para la tabla `permisos`
+--
+ALTER TABLE `permisos`
+  ADD CONSTRAINT `permisos_ibfk_1` FOREIGN KEY (`id_opcion`) REFERENCES `opciones` (`id_opcion`);
+
+--
+-- Filtros para la tabla `restricciones`
+--
+ALTER TABLE `restricciones`
+  ADD CONSTRAINT `restricciones_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`),
+  ADD CONSTRAINT `restricciones_ibfk_2` FOREIGN KEY (`id_opcion`) REFERENCES `opciones` (`id_opcion`);
+
+--
+-- Filtros para la tabla `roles_menus`
+--
+ALTER TABLE `roles_menus`
+  ADD CONSTRAINT `roles_menus_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`),
+  ADD CONSTRAINT `roles_menus_ibfk_2` FOREIGN KEY (`id_menu`) REFERENCES `menus` (`id_menu`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `personas` (`id_persona`),
+  ADD CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
